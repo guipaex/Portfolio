60945805dc584d1fe6e1bc3f533b3f2a8a695c12
Guilherme Paes <paes.guilherme@hotmail.com>
2023-06-30 09:33:57 -0300

    responsividade aprimorada

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
         <a href='https://github.com/guipaex' target='_blank'>
           <FaGithub className={styles.listaLinks__link} />
         </a>
-        <a href=''>
+        <a href='https://www.linkedin.com/in/guilhermepaes/' target='_blank'>
           <FaLinkedin className={styles.listaLinks__link} />
         </a>
-        <a href=''>
+        <a href='mailto:guipaex@gmail.com' target='_blank'>
           <FaRegEnvelope className={styles.listaLinks__link} />
         </a>
-        <a href=''>
+        <a href='https://www.behance.net/guilhermepaes' target='_blank'>
           <FaBehance className={styles.listaLinks__link} />
         </a>
       </ul>
