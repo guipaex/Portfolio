8cb7ec2ba7ba7cb5535b5413eb4217967ea16825
Guilherme Paes <paes.guilherme@hotmail.com>
2023-06-30 10:32:57 -0300

    correção de bugs

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,6 +4,13 @@ import { Behance, Email, Github, Linkedin } from "../../components/Buttons";
 import { FaGithub, FaLinkedin, FaRegEnvelope, FaBehance, FaAngleDown, FaChevronDown } from "react-icons/fa";
 
 export default function Home() {
+  function scrollTop() {
+    window.scrollTo({
+      left: 0,
+      top: 900,
+      behavior: "smooth",
+    });
+  }
   return (
     <>
       <div className={styles.container}>
@@ -16,7 +23,7 @@ export default function Home() {
             <Behance />
             <Email />
           </ul>
-          <FaChevronDown className={styles.skipper__arrow} />
+          <FaChevronDown onClick={scrollTop} className={styles.skipper__arrow} />
         </div>
       </div>
       <div className={styles.logo__container}>
