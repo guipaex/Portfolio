07990955778162a042f9716b485bd584eefcde57
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-04 01:37:41 -0300

    style: update about section styles

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./About.module.scss";
 import MyPhoto from "../../assets/guipaex.png";
-import { FaLinkedin, FaGithub, FaBehance } from "react-icons/fa6";
+import { FaLinkedin, FaGithub, FaBehance, FaArrowDown } from "react-icons/fa6";
 import {
   SiHtml5,
   SiCss3,
@@ -16,10 +16,25 @@ import {
   SiGithub,
 } from "react-icons/si";
 export default function About() {
+  const techIcons = [
+    <SiHtml5 className={styles.techList__item} />,
+    <SiCss3 className={styles.techList__item} />,
+    <SiJavascript className={styles.techList__item} />,
+    <SiTypescript className={styles.techList__item} />,
+    <SiReact className={styles.techList__item} />,
+    <SiMysql className={styles.techList__item} />,
+    <SiMongodb className={styles.techList__item} />,
+    <SiGit className={styles.techList__item} />,
+    <SiNodedotjs className={styles.techList__item} />,
+    <SiSass className={styles.techList__item} />,
+    <SiFigma className={styles.techList__item} />,
+    <SiGithub className={styles.techList__item} />,
+  ];
+
   return (
     <section id='about' className={styles.container}>
       <h2 className={styles.title}>
-        More <span className={styles.highlight}>about me</span>
+        More <span className={styles.title__highlight}>about me</span>
         <span className={styles.title__dot}>.</span>
       </h2>
       <div className={styles.content}>
@@ -47,33 +62,24 @@ export default function About() {
             in a lot of advertising agencies. Now, as a <span className={styles.highlight}>Web Developer</span>, I use
             my experience to design build beautiful interfaces.
           </p>
-          <button>
-            <a
-              className={styles.resume}
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://drive.google.com/file/d/19l2tbklUnSWXVQcW9Y8cSPNs1T2bL2Ou/view?usp=sharing'
-            >
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://drive.google.com/file/d/1GMER6wKM_QLWpBuxfK2y13cTB_RZfp5Z/view?usp=sharing'
+          >
+            <div className={styles.resumeBtn}>
               Download Resume
-            </a>
-          </button>
+              <FaArrowDown />
+            </div>
+          </a>
         </div>
 
         <p className={styles.text}>These are some of technologies that I most use:</p>
-        <div className={styles.listaStack}>
-          <SiHtml5 className={styles.listaStack__item} />
-          <SiCss3 className={styles.listaStack__item} />
-          <SiJavascript className={styles.listaStack__item} />
-          <SiTypescript className={styles.listaStack__item} />
-          <SiReact className={styles.listaStack__item} />
-          <SiSass className={styles.listaStack__item} />
-          <SiGithub className={styles.listaStack__item} />
-          <SiFigma className={styles.listaStack__item} />
-          <SiMysql className={styles.listaStack__item} />
-          <SiMongodb className={styles.listaStack__item} />
-          <SiGit className={styles.listaStack__item} />
-          <SiNodedotjs className={styles.listaStack__item} />
-        </div>
+        <ul className={styles.techList}>
+          {techIcons.map((icon) => {
+            return icon;
+          })}
+        </ul>
       </div>
     </section>
   );
