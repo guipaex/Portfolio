21b06fb1ddcfebf51e5e094f2e19a28337c38915
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-03 23:37:38 -0300

    style: add background VFX

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import About from "./Pages/About";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
 import Footer from "./components/Footer";
+import styles from "./index.module.scss";
 
 function App() {
   return (
-    <>
+    <main>
       <Header />
       <Home />
       <About />
       <Projects />
       <Contact />
       <Footer />
-    </>
+      <span className={styles.bgFX} />
+    </main>
   );
 }
 
