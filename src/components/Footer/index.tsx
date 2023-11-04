bcdf673d78c67b21680e0b82af067dc261417539
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-03 22:54:41 -0300

    style: update footer style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer className={styles.container}>
+    <footer>
       © 2023 | Designed and Developed by{" "}
       <a className={styles.link} href='https://github.com/guipaex' target='_blank' rel='noreferrer'>
         Guilherme Paes
