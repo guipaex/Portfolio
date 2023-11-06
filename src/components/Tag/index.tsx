b8c75bec7159d98e205bc317b15ebba14fc0c308
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-05 22:21:45 -0300

    style: Tag colore

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Tag = styled.li`
   padding: 0.5rem 0.75rem;
-  background-color: #013838;
+  background-color: #434343;
   font-size: 0.8rem;
   border-radius: 0.5rem;
 `;
