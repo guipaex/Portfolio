77b45ea5d4b542b79e8bd92633609313cfa8d18b
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-05 22:23:32 -0300

    style: Tag style

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -3,8 +3,10 @@ import styled from "styled-components";
 const Tag = styled.li`
   padding: 0.5rem 0.75rem;
   background-color: #434343;
-  font-size: 0.8rem;
+  font-size: 0.7rem;
+  font-weight: 600;
   border-radius: 0.5rem;
+  text-transform: uppercase;
 `;
 
 export default Tag;
